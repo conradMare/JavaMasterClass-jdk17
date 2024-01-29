ef6f659ea6ff3af5744177592b4a87ae6dfda7e6
conradMare <conradmare4@gmail.com>
2024-01-29 08:04:17 +0200

    2024/01/29(am)
    Coding Exercise 01 - Positive, Negative or Zero

---
+++ S05_ExpressionsStatementsMore/codingExercise1_PositiveNegativeOrZero/src/PositiveNegativeZero.java
@@ -0,0 +1,14 @@
+public class PositiveNegativeZero {
+    // write code here
+    public static void checkNumber(int number) {
+
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+        // System.out.println((number > 0) ? "positive" : ((number < 0) ? "negative" : "zero"));
+    }
+}
