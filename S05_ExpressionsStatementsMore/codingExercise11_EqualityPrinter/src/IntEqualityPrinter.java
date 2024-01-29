c7bd7dcf8dad86724132a1c4cb45ccbb82038635
conradMare <conradmare4@gmail.com>
2024-01-29 08:57:15 +0200

    2024/01/29(am)
    Coding Exercise 11 - Equality Printer

---
+++ S05_ExpressionsStatementsMore/codingExercise11_EqualityPrinter/src/IntEqualityPrinter.java
@@ -0,0 +1,20 @@
+public class IntEqualityPrinter {
+    public static void main(String[] args) {
+
+        printEqual(6, 12, 18);
+    }
+    public static void printEqual(int x, int y, int z) {
+        if (x < 0 || y < 0 || z < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        if (x == y && y == z) {
+            System.out.println("All numbers are equal");
+        } else if (x == y || y == z || z == x) {
+            System.out.println("Neither all are equal or different");
+        } else {
+            System.out.println("All numbers are different");
+        }
+    }
+}
