4f454975f99036bd98f23222b1366cd7eac91e03
conradMare <conradmare4@gmail.com>
2024-02-02 07:22:18 +0200

    2024/02/02(am)
    Coding Exercise 49 - Playlist_InnerClass.
    (Incomplete)

---
+++ S13_NestedClassesAndTypes/codingExercise49_Playlist.InnerClass/src/coding/exercise49/Main.java
@@ -0,0 +1,9 @@
+package coding.exercise49;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var Song = new Song("A", 3.12);
+        System.out.println(Song.getTitle());
+    }
+}
---
+++ S13_NestedClassesAndTypes/codingExercise49_Playlist.InnerClass/src/coding/exercise49/Song.java
@@ -0,0 +1,10 @@
+package coding.exercise49;
+
+public class Song {
+    public Song(String title, double duration) {
+    }
+
+    public String getTitle() {
+        return null;
+    }
+}
