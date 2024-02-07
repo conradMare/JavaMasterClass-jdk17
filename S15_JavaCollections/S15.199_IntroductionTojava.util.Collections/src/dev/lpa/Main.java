778e58b8dbe710c2dbfa1ad8787409a0208a0014
conradMare <conradmare4@gmail.com>
2024-02-07 11:05:42 +0200

    2024/02/07(am)
    S15.199_IntroductionTojava.util.Collections - method descriptions added

--- S15_JavaCollections/S15.199_IntroductionTojava.util.Collections/src/dev/lpa/Main.java
+++ S15_JavaCollections/S15.199_IntroductionTojava.util.Collections/src/dev/lpa/Main.java
@@ -1,11 +1,47 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
 
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card Collection with Aces added", 2);
+
+//        This method copies elements from one list to another, doesn't return a copy of the list.
+//        Elements are being assigned to the destination list VS. a copy being made
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards, "Card Collection with Kings copied", 2);
+
+//        This method returns an unmodifiable list.
+//        If in need of a modifiable list, make a call to the copyOf method and pass it to a list constructor or use addAll for example.
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List Copy of Kings", 1);
+
     }
 }
