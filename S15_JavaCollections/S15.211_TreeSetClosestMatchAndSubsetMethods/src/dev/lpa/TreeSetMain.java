40014a2080a43af63eae6e0de7eaa45f89c687e3
conradMare <conradmare4@gmail.com>
2024-02-12 07:35:17 +0200

    2024/02/12(am)
    S15.211_TreeSetClosestMatchAndSubsetMethods

---
+++ S15_JavaCollections/S15.211_TreeSetClosestMatchAndSubsetMethods/src/dev/lpa/TreeSetMain.java
@@ -0,0 +1,94 @@
+package dev.lpa;
+
+import java.util.*;
+
+public class TreeSetMain {
+
+    public static void main(String[] args) {
+
+        List<Contact> phones = ContactData.getData("phone");
+        List<Contact> emails = ContactData.getData("email");
+
+//        NavigableSet<Contact> sorted = new TreeSet<>(phones);
+        Comparator<Contact> mySort = Comparator.comparing(Contact::getName);
+        NavigableSet<Contact> sorted = new TreeSet<>(mySort);
+        sorted.addAll(phones);
+        sorted.forEach(System.out::println);
+
+        NavigableSet<String> justNames = new TreeSet<>();
+        phones.forEach(c -> justNames.add(c.getName()));
+        System.out.println(justNames);
+
+        NavigableSet<Contact> fullSet = new TreeSet<>(sorted);
+        fullSet.addAll(emails);
+        fullSet.forEach(System.out::println);
+
+        List<Contact> fullList = new ArrayList<>(phones);
+        fullList.addAll(emails);
+        fullList.sort(sorted.comparator());
+        System.out.println("-".repeat(45));
+        fullList.forEach(System.out::println);
+
+        Contact min = Collections.min(fullSet, fullSet.comparator());
+        Contact max = Collections.max(fullSet, fullSet.comparator());
+
+        Contact first = fullSet.first();
+        Contact last = fullSet.last();
+
+        System.out.println("-".repeat(45));
+        System.out.printf("min = %s, first=%s %n", min.getName(), first.getName());
+        System.out.printf("max = %s, last=%s %n", max.getName(), last.getName());
+        System.out.println("-".repeat(45));
+
+        NavigableSet<Contact> copiedSet = new TreeSet<>(fullSet);
+        System.out.println("First element = " + copiedSet.pollFirst());
+        System.out.println("Last element = " + copiedSet.pollLast());
+        copiedSet.forEach(System.out::println);
+        System.out.println("-".repeat(45));
+
+        Contact daffy = new Contact("Daffy Duck");
+        Contact daisy = new Contact("Daisy Duck");
+        Contact snoopy = new Contact("Snoopy");
+        Contact archie = new Contact("Archie");
+
+        for (Contact c : List.of(daffy, daisy, last, snoopy)) {
+            System.out.printf("ceiling(%s)=%s%n", c.getName(), fullSet.ceiling(c));
+            System.out.printf("higher(%s)=%s%n", c.getName(), fullSet.higher(c));
+        }
+        System.out.println("-".repeat(45));
+
+        for (Contact c : List.of(daffy, daisy, first, archie)) {
+            System.out.printf("floor(%s)=%s%n", c.getName(), fullSet.floor(c));
+            System.out.printf("lower(%s)=%s%n", c.getName(), fullSet.lower(c));
+        }
+        System.out.println("-".repeat(45));
+
+        NavigableSet<Contact> descendingSet = fullSet.descendingSet();
+        descendingSet.forEach(System.out::println);
+        System.out.println("-".repeat(45));
+
+        Contact lastContact = descendingSet.pollLast();
+        System.out.println("Removed " + lastContact);
+        descendingSet.forEach(System.out::println);
+        System.out.println("-".repeat(45));
+        fullSet.forEach(System.out::println);
+        System.out.println("-".repeat(45));
+
+        Contact marion = new Contact("Maid Marion");
+        var headSet = fullSet.headSet(marion, true);
+        headSet.forEach(System.out::println);
+        System.out.println("-".repeat(45));
+
+//        Overloaded method to change the default behavior (boolean value == false):
+        var tailSet = fullSet.tailSet(marion, false);
+        tailSet.forEach(System.out::println);
+        System.out.println("-".repeat(45));
+
+//        Two overloaded methods to change the default behavior (boolean value == false && boolean value == true):
+        Contact linus = new Contact("Linus Van Pelt");
+        var subset = fullSet.subSet(
+                linus, false, marion, true);
+        subset.forEach(System.out::println);
+
+    }
+}
