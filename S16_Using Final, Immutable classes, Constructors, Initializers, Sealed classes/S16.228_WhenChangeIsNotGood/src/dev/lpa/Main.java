8c30f6950b964eec9843ff378f7897f838272886
conradMare <conradmare4@gmail.com>
2024-02-17 07:15:30 +0200

    2024/02/17(am)
    S16.228_WhenChangeIsNotGood

---
+++ S16_Using Final, Immutable classes, Constructors, Initializers, Sealed classes/S16.228_WhenChangeIsNotGood/src/dev/lpa/Main.java
@@ -0,0 +1,54 @@
+package dev.lpa;
+
+import consumer.specific.ChildClass;
+import dev.lpa.generic.BaseClass;
+import external.util.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BaseClass parent = new BaseClass();
+        ChildClass child = new ChildClass();
+        BaseClass childReferredToAsBase = new ChildClass();
+
+        parent.recommendedMethod();
+        System.out.println("-".repeat(45));
+        childReferredToAsBase.recommendedMethod();
+        System.out.println("-".repeat(45));
+        child.recommendedMethod();
+
+        System.out.println("-".repeat(45));
+        parent.recommendedStatic();
+        System.out.println("-".repeat(45));
+        childReferredToAsBase.recommendedStatic();
+        System.out.println("-".repeat(45));
+        child.recommendedStatic();
+
+        System.out.println("-".repeat(45));
+        BaseClass.recommendedStatic();
+        ChildClass.recommendedStatic();
+
+        String xArgument = "This is all I've got to say about Section ";
+        StringBuilder zArgument = new StringBuilder("Only saying this: Section ");
+        doXYZ(xArgument, 16, zArgument);
+        System.out.println("After Method, xArgument: " + xArgument);
+        System.out.println("After Method, zArgument: " + zArgument);
+
+//        Defensive coding:
+        StringBuilder tracker = new StringBuilder("Step 1 is abc");
+        Logger.logToConsole(tracker.toString());
+        tracker.append(", Step 2 is xyz.");
+        Logger.logToConsole(tracker.toString());
+        System.out.println("After logging, tracker = " + tracker);
+    }
+
+    private static void doXYZ(String x, int y, final StringBuilder z) {
+
+        final String c = x + y;
+        System.out.println("c = " + c);
+        x = c;
+        z.append(y);
+//        z = new StringBuilder("This is a new reference");
+    }
+}
