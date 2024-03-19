fbc3f2fb760143e482e5639fdcf6075b840050c6
conradMare <conradmare4@gmail.com>
2024-03-19 07:53:41 +0200

    2024/03/19(am)
    ManagingFiles - S20.311_Renaming, copying, deleting files and directories && S20.312_Copying, Deleting Directories_InputStream & Reader's transferTo method

---
+++ S20_Input & Output (I/ManagingFiles/src/dev/lpa/Main.java
@@ -0,0 +1,83 @@
+package dev.lpa;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        File oldFile = new File("students.json");
+//        File newFile = new File("student-activity.json");
+//        if (oldFile.exists()) {
+//            oldFile.renameTo(newFile);
+//            System.out.println("File renamed successfully!");
+//        } else {
+//            System.out.println("File does not exist!");
+//        }
+
+//        Path oldPath = Path.of("students.json");
+//        Path newPath = Path.of("files/student-activity.json");
+//
+//        try {
+//            Files.createDirectories(newPath.subpath(0, newPath.getNameCount() - 1));
+//            Files.move(oldPath, newPath);
+//            System.out.println(oldPath + " moved (renamed to) --> " + newPath);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        Path fileDir = Path.of("files");
+        Path resourceDir = Path.of("resources");
+        try {
+            recurseDelete(resourceDir);
+            recurseCopy(fileDir, resourceDir);
+            System.out.println("Directory copied to " + resourceDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        
+    }
+
+    public static void recurseCopy(Path source, Path target) throws IOException {
+
+        // Shallow copy
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+        if (Files.isDirectory(source)) {
+            try (var children = Files.list(source)) {
+                children.toList().forEach(
+                        p -> {
+                            try {
+                                Main.recurseCopy(
+                                        p, target.resolve(p.getFileName())
+                                );
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+            }
+        }
+    }
+
+    public static void recurseDelete(Path target) throws IOException {
+
+        if (Files.isDirectory(target)) {
+            try (var children = Files.list(target)) {
+                children.toList().forEach(
+                        p -> {
+                            try {
+                                Main.recurseDelete(p);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+            }
+        }
+        Files.delete(target);
+    }
+}
