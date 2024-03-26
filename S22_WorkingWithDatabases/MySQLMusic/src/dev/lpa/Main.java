5a4f3d031b26909d1ac6395078e94a6bb2162516
conradMare <conradmare4@gmail.com>
2024-03-26 11:48:44 +0200

    2024/03/26(am)
    MySQLMusic - S22.357_Connecting to the music database

---
+++ S22_WorkingWithDatabases/MySQLMusic/src/dev/lpa/Main.java
@@ -0,0 +1,42 @@
+package dev.lpa;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final String CONN_STRING = "jdbc:mysql://localhost:3306/music";
+
+        String username = JOptionPane.showInputDialog(
+                null, "Enter DB Username");
+
+        JPasswordField pf = new JPasswordField();
+        int okCxl = JOptionPane.showConfirmDialog(null, pf,
+                "Enter DB Password", JOptionPane.OK_CANCEL_OPTION);
+
+        final char[] password =
+                (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
+
+        var dataSource = new MysqlDataSource();
+//        dataSource.setURL(CONN_STRING);
+        dataSource.setServerName("localhost");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("music");
+
+//        try (Connection connection = DriverManager.getConnection(
+//                CONN_STRING, username, String.valueOf(password))) {
+        try(Connection connection = dataSource.getConnection(
+                username, String.valueOf(password)
+        )) {
+            System.out.println("Success!! Connection made to the music db");
+            Arrays.fill(password, ' ');
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
