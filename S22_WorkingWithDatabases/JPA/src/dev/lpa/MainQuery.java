296618164441a796adec060c3351d2b10698c106
conradMare <conradmare4@gmail.com>
2024-04-05 08:53:33 +0200

    2024/04/05(am)
    JPA - S22.376_JPA Queries, JPQL Joins.

--- S22_WorkingWithDatabases/JPA/src/dev/lpa/MainQuery.java
+++ S22_WorkingWithDatabases/JPA/src/dev/lpa/MainQuery.java
@@ -4,8 +4,10 @@ import dev.lpa.music.Artist;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Tuple;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class MainQuery {
 
@@ -18,8 +20,16 @@ public class MainQuery {
 
             var transaction = em.getTransaction();
             transaction.begin();
-            artists = getArtistsJPQL(em, "%Stev%");
+            artists = getArtistsJPQL(em, "%Greatest Hits%");
             artists.forEach(System.out::println);
+
+//            var names = getArtistNames(em, "%Stev%");
+//            names
+//                    .map(a -> new Artist(
+//                            a.get("id", Integer.class),
+//                            (String) a.get("name")))
+//                    .forEach(System.out::println);
+
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -28,9 +38,21 @@ public class MainQuery {
 
     private static List<Artist> getArtistsJPQL(EntityManager em, String matchedValue) {
 
-        String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE ?1";
+//        String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE ?1";
+        String jpql = "SELECT a FROM Artist a JOIN albums album " +
+                "WHERE album.albumName LIKE ?1 OR album.albumName LIKE ?2";
         var query = em.createQuery(jpql, Artist.class);
         query.setParameter(1, matchedValue);
+        query.setParameter(2, "%Best of%");
         return query.getResultList();
+    }
+
+    private static Stream<Tuple> getArtistNames(EntityManager em, String matchedValue) {
+
+        String jpql = "SELECT a.artistId id, a.artistName as name FROM Artist a " +
+                "WHERE a.artistName LIKE ?1";
+        var query = em.createQuery(jpql, Tuple.class);
+        query.setParameter(1, matchedValue);
+        return query.getResultStream();
     }
 }
